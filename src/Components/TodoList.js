08590832a2e1c5fa8546af7fa374b2dfad11ae1f
Sachin Lohani <schnlohani@gmail.com>
2020-03-02 11:38:43 +0530

    editing

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,42 +1,50 @@
 import React, { useState } from "react";
+let counter = 0;
+let todo = { id: 1, title: "string" };
 
-const items=[];
 let TodoList = () => {
-   
   const [state, setState] = useState("");
+  const items = [{ id: counter, value: state }];
   const [list, setList] = useState(items);
-  const [color , setColor] = useState("yellow");
+  const [color, setColor] = useState("yellow");
 
   let handlechange = e => {
-    setState(e.target.value);
+    setState({ id: counter, value: e.target.value });
   };
- 
-  let addItem = (e)=>{
-    if (state){
-      setList( list.concat(state));
-     
+
+  let addItem = e => {
+    counter++;
+    if (state) {
+      setList(list.concat(state));
     }
     e.preventDefault();
     console.log(list);
-  }
+  };
 
-  let btnClass = (color==="yellow")?"yellow": "Item";
+  let btnClass = color === "yellow" ? "yellow" : "Item";
 
-  let oncolorChange=()=>{
-    setColor("");
-  }
+  let oncolorChange = itemId => {
+    console.log("Inside button click");
+    list.map(id => {
+      console.log(id);
+      console.log(itemId);
+      // id === itemId ? setColor("") : null;
+    });
+  };
 
   return (
     <div>
-      <form onSubmit ={addItem} >
-      <input type="text" onChange={handlechange} />
-      <button>Change Input</button>
+      <form onSubmit={addItem}>
+        <input type="text" onChange={handlechange} />
+        <button>Add item</button>
       </form>
       <ul>
-  {list.map((item)=><li key={item} className={btnClass}> 
-    {item} <br/>
-    <button onClick={oncolorChange}>Completed</button>
-    </li> )}
+        {list.map(item => (
+          <li key={item.id} className={btnClass}>
+            {item.value} <br />
+            <button onClick={oncolorChange(item.id)}>Completed</button>
+          </li>
+        ))}
       </ul>
     </div>
   );
